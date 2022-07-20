873f98d4da63618559a41b95c647cc47ff89c3ed
jpatrick5402 <jpatrick5402@gmail.com>
2022-07-20 18:01:12 -0400

    Saving after detecting face

--- Python/Main.py
+++ Python/Main.py
@@ -1,6 +1,8 @@
 #pip install opencv-python
 import cv2
 import json
+from datetime import datetime
+from numpy import array, ndarray
 
 def Save(): #Saves image data usig json format
     pass
@@ -10,6 +12,9 @@ def main():
 	h = 0
 	x = 0
 	w = 0
+
+	test = 0
+
 	cap = cv2.VideoCapture()
 	# The device number might be 0 or 1 depending on the device and the webcam
 	cap.open(0, cv2.CAP_DSHOW)
@@ -24,6 +29,13 @@ def main():
 		ret, frame = cap.read()
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+		if test > 100:
+			print("Face Detected")
+			time = "data/Captures/Capture_" + str(datetime.now()).replace(":", "-") + ".jpg"
+			print(time)
+			cv2.imwrite(time, frame)
+			test = 0
+
 		faces = faceCascade.detectMultiScale(
 				gray,
 				scaleFactor=1.1,
@@ -34,6 +46,9 @@ def main():
 
 		for (x, y, w, h) in faces:
 			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+		
+
 		faceROI = gray[y:y+h,x:x+w]
 		eyes = eyeCascade.detectMultiScale(faceROI)
 		for (x2,y2,w2,h2) in eyes:
@@ -41,11 +56,23 @@ def main():
 			radius = int(round((w2 + h2)*0.25))
 			frame = cv2.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
 
+		
+
 		side = profileCascade.detectMultiScale(gray)
 		for (x, y, w, h) in side:
 			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
+		
+
 		cv2.imshow('frame', frame)
+
+		if type(faces) == ndarray or type(eyes) == ndarray or type(side) == ndarray:
+			test = test + 3
+		else:
+			test = test - 1
+
+
+		
 
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
