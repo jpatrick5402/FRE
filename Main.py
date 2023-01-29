f3545b4e7abcd6fe71686da0be9be0a141d05b75
jpatrick5402 <jpatrick5402@gmail.com>
2023-01-29 14:29:07 -0500

    adjusting files

--- Main.py
+++ Main.py
@@ -4,10 +4,14 @@ import json
 from datetime import datetime
 from numpy import array, ndarray
 
+def main():
+	showVid(drawLines=True)
+	return 0
+
 def Save(): #Saves image data usig json format
     pass
 
-def main():
+def showVid(drawLines=False):
 	y = 0
 	h = 0
 	x = 0
@@ -19,73 +23,59 @@ def main():
 	# The device number might be 0 or 1 depending on the device and the webcam
 	cap.open(0, cv2.CAP_DSHOW)
 
-	faceCascade = cv2.CascadeClassifier("data\haarcascades\haarcascade_frontalface_default.xml")
+	ret, frame = cap.read()
+	if drawLines:
 
-	eyeCascade = cv2.CascadeClassifier("data\haarcascades\haarcascade_eye_tree_eyeglasses.xml")
+		faceCascade = cv2.CascadeClassifier(".\haarcascade_frontalface_default.xml")
 
-	profileCascade = cv2.CascadeClassifier("data\haarcascades\haarcascade_profileface.xml")
+		eyeCascade = cv2.CascadeClassifier(".\haarcascade_eye_tree_eyeglasses.xml")
 
-	fullCascade = cv2.CascadeClassifier("data\haarcascades\haarcascade_fullbody.xml")
+		profileCascade = cv2.CascadeClassifier(".\haarcascade_profileface.xml")
 
+		fullCascade = cv2.CascadeClassifier(".\haarcascade_fullbody.xml")
 
-	while(True):
-		ret, frame = cap.read()
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		print(test)
-		if test > 20:
-			print("Face Detected")
-			time = "data/Captures/Capture_" + str(datetime.now()).replace(":", "-") + ".jpg"
-			print(time)
-			cv2.imwrite(time, frame)
-			test = 0
+		while(True):
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-		faces = faceCascade.detectMultiScale(
-				gray,
-				scaleFactor=1.1,
-				minNeighbors=5,
-				minSize=(30, 30)
-				
-			)
+			faces = faceCascade.detectMultiScale(
+					gray,
+					scaleFactor=1.1,
+					minNeighbors=5,
+					minSize=(30, 30)
+				)
 
-		for (x, y, w, h) in faces:
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+			for (x, y, w, h) in faces:
+				cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-		
+			faceROI = gray[y:y+h,x:x+w]
+			eyes = eyeCascade.detectMultiScale(faceROI)
+			for (x2,y2,w2,h2) in eyes:
+				eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+				radius = int(round((w2 + h2)*0.25))
+				frame = cv2.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
 
-		faceROI = gray[y:y+h,x:x+w]
-		eyes = eyeCascade.detectMultiScale(faceROI)
-		for (x2,y2,w2,h2) in eyes:
-			eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-			radius = int(round((w2 + h2)*0.25))
-			frame = cv2.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
+			side = profileCascade.detectMultiScale(gray)
+			for (x, y, w, h) in side:
+				cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-		
+			full = fullCascade.detectMultiScale(gray)
+			for (x, y, w, h) in full:
+				cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-		side = profileCascade.detectMultiScale(gray)
-		for (x, y, w, h) in side:
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+			cv2.imshow('VideoCapture', frame)
 
-		full = fullCascade.detectMultiScale(gray)
-		for (x, y, w, h) in full:
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+	else:
+		while True:
+			cv2.imshow('VideoCapture', frame)
 
-		cv2.imshow('VideoCaputure', frame)
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
 
-		if type(faces) == ndarray or type(eyes) == ndarray or type(side) == ndarray or type(full) == ndarray:
-			test = test + 3
-		elif test > 0:
-			test = test - 1
-
-
-		
-
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
 
 	cap.release()
 	cv2.destroyAllWindows()
-
-
 
 if __name__ == "__main__":
 	main()
